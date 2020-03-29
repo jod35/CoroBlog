2eb22302f7b8f9c3043961ea93fedfe6e3683f5e
jona <jona@jona.com>
2020-03-29 16:50:29 +0300

    post model created

--- simple/models.py
+++ simple/models.py
@@ -8,6 +8,7 @@ class User(db.Model,UserMixin):
     username=db.Column(db.String(255),nullable=False)
     email=db.Column(db.String(255),nullable=False)
     password=db.Column(db.Text,nullable=False)
+    posts=db.relationship('Post',backref='author',lazy=True)
 
     def __repr__(self):
         return self.username
@@ -15,3 +16,12 @@ class User(db.Model,UserMixin):
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+
+class Post(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    post_comment=db.Column(db.String(140),nullable=False)
+    user_id=db.Column(db.Integer(),db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return "{}".format(self.post_comment)
